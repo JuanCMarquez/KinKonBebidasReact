62fca71f587b55e9f5ccd33f80f4324673407b3b
Juan Cruz <juancmarquez90@gmail.com>
2024-02-24 12:08:43 -0300

    Mejoro black navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
   return (
     <nav className='bg-black py-4 w-full'>
       <div className='container mx-auto flex justify-between items-center'>
-        <div className='h-[20%] w-[20%] p-0 items-left'>
+        <div className='h-[24%] w-[24%] p-0 items-left'>
           <img src="./src/assets/images/logokikonblanco2.png" alt="Icono Gorila Color" />
         </div>
         <div className='flex justify-center items-center flex-grow'>
-          <img className='' src="" alt="" />
-          <form className='bg-white p-3 h-[25%] w-[45%] rounded'>
+          <img className='p-3 h-[6.5%] w-[6.5%]' src="src/assets/images/lupablanca2.png" alt="lupaNav" />
+          <form className='bg-white p-3 h-[25%] w-[65%] w-1/2 rounded'>
             <input className='bg-white p-1 h-[20%] w-full type="text"'/>
           </form>
         </div>
         <ul>
-          <li>Productos</li>
-          <li>Contacto</li>
-          <li>Carrito</li>
+          <li className='text-white'>Productos</li>
+          <li className='text-white'>Contacto</li>
+          <li className='text-white'>Carrito</li>
         </ul>
       </div>
     </nav>
