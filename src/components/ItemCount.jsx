b54684a33b5f96eec467c155344d19c0dd2cdb83
Juan Cruz <juancmarquez90@gmail.com>
2024-03-16 12:12:53 -0300

    Mejoro lógica de contador

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -2,35 +2,37 @@
 import React, { useState } from "react";
 
 const ItemCount = ({ stock, initial, onAdd }) => {
-  const [count, setCount] = useState(initial)
+  const [count, setCount] = useState(initial);
 
   const handleCountChange = (value) => {
     const newCount = count + value;
-    if (newCount >= 1 && newCount <= stock) {
-      setCount(newCount)
+    if (newCount >= 0 && newCount <= stock) {
+      setCount(newCount);
+    } else if (newCount < 0) {
+      setCount(0);
     }
-  }
+  };
 
   const handleAdd = () => {
     if (count > 0) {
-      onAdd(count)
+      onAdd(count);
     }
+  };
 
-  }
-
-
-  return (<div>
-    <button onClick={() => handleCountChange(-1)}>
-      -
-    </button>
-    <span>{count}</span>
-    <button onClick={() => handleCountChange(+1)}>
-      +
-    </button>
-    <button onClick={() => handleAdd()}>
-      Agregar al carrito
-    </button>
-  </div>);
-}
+  return (
+    <div>
+      <button onClick={() => handleCountChange(-1)} disabled={count <= 0}>
+        -
+      </button>
+      <span>{count}</span>
+      <button onClick={() => handleCountChange(+1)} disabled={count >= stock}>
+        +
+      </button>
+      <button onClick={handleAdd} disabled={count <= 0}>
+        Agregar al carrito
+      </button>
+    </div>
+  );
+};
 
 export default ItemCount;
