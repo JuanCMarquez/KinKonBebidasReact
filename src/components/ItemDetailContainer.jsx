2172ea8bc971165564b0f01e3c1c1f00d7e386aa
Juan Cruz <juancmarquez90@gmail.com>
2024-03-16 16:37:27 -0300

    Corrijo find products con profe y tutor

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -2,19 +2,19 @@
 
 import React, { useEffect, useState } from "react";
 import products from '../utils/MocksAsync.json';
-import { fakeApiCall } from '../utils/fakeApiCall';
+import { fakeApiCallById } from '../utils/fakeApiCall';
 import { ItemDetail } from "./ItemDetail";
+import { Link, useParams } from "react-router-dom";
 
 
 const ItemDetailContainer = () => {
   const [productsCharged, setProductsCharged] = useState({})
   const [loading, setLoading] = useState(true)
-
-
+  const { id } = useParams();
 
   useEffect(() => {
     setLoading(true)
-    fakeApiCall(products).then(resp => { setProductsCharged(resp); setLoading(false) })
+    fakeApiCallById(products, id).then(resp => { setProductsCharged(resp); setLoading(false) })
 
 
   }, [])
@@ -26,12 +26,12 @@ const ItemDetailContainer = () => {
 
     <div className='text-white text-xl flex items-center justify-center p-2'>
       {
-        productsCharged.productos.length > 0 && productsCharged.productos.map((item, index) => {
-          return <ItemDetail item={item} />
-        }
-
-        )
+        //productsCharged.productos.length > 0 && productsCharged.productos((item) => {
+        //return <ItemDetail item={item} />
       }
+
+      <ItemDetail item={productsCharged} />
+
     </div>
 
   </>);
--- src/utils/fakeApiCall.js
+++ src/utils/fakeApiCall.js
@@ -1,11 +1,17 @@
-export const fakeApiCall = (mock, categoryId) => {
-  return new Promise((resolve, reject) => {
+export const fakeApiCall = (mock) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      if (!categoryId) {
-        resolve(mock)
-      }
-      resolve(mock.productos.filter((prod) => prod.categoria.toString() === categoryId))
-    }
-      , 1000)
-  })
-}
+      resolve(mock);
+    }, 1000);
+  });
+};
+
+export const fakeApiCallById = (mock, id) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(mock.productos.find(prod => prod.id.toString() === id))
+    }, 1000);
+  });
+};
+
+
