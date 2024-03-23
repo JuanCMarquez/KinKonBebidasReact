bc045951adcd287e3be4c1f6768d917511e3b08b
Juan Cruz <juancmarquez90@gmail.com>
2024-03-23 01:41:16 -0300

    Mejoro estilos de Item Detail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -11,9 +11,9 @@ const ItemDetail = ({ item }) => {
       <div className="mx-auto my-auto">
         <div className="card text-center" style={{ width: '18rem' }}>
           <div className="card-body">
-            <h3 className="card-title fw-bold">{item.nombre}</h3>
-            <p className="card-text">Precio: {item.precio}</p>
-            <p className="card-text">Descripción: {item.descripcion}</p>
+            <h3 className="card-title fw-bold p-1 text-2xl ">{item.nombre}</h3>
+            <p className="card-text p-2">Precio: {item.precio}</p>
+            <p className="card-text p-2">Descripción: {item.descripcion}</p>
             <div className="d-flex justify-content-between align-items-center">
               <ItemCount stock={item.stock} initial={0} onAdd={handleAdd} />
             </div>
