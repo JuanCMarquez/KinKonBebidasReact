45ab83e2bda3dafd20685d0bdfc016373ba2d672
Juan Cruz <juancmarquez90@gmail.com>
2024-04-16 23:46:35 -0300

    Navbar por NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import NavBar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import './index.css';
