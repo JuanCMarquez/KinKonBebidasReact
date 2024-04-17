9a41c0cc3a52d8ad3727bf1c0c32b09e1603f586
Juan Cruz <juancmarquez90@gmail.com>
2024-04-17 00:11:00 -0300

    Procuro corregir el error de deploy de Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar.jsx';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import './index.css';
