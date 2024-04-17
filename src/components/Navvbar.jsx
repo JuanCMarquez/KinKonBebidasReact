6a6d1585e55fa32be0cebc5ef3448d0a0a91edc0
Juan Cruz <juancmarquez90@gmail.com>
2024-04-17 01:53:09 -0300

    Mejoro estilos nav

--- src/components/Navvbar.jsx
+++ src/components/Navvbar.jsx
@@ -45,10 +45,10 @@ const Navbar = ({ loading }) => {
       <div className='container mx-auto flex justify-between items-center flex-grow'>
         <div className='h-[24%] w-[24%] p-0 items-left'>
           <Link to="/">
-            <img src={logo} alt="Icono Gorila Color" className="h-auto w-24 sm:w-32 md:w-40 lg:w-48 xl:w-56" />
+            <img src={logo} alt="Icono Gorila Color" className="h-auto w-24 sm:w-32 md:w-40 lg:w-48 xl:w-56 flex-grow" />
           </Link>
         </div>
-        <div className='hidden md:flex justify-center items-center flex-grow'>
+        <div className='hidden lg:flex justify-center items-center flex-grow'>
           <button className='p-3 h-auto w-auto border-none bg-transparent'>
             <img src={lupa} alt="lupaNav" className="h-auto w-6 sm:w-8 md:w-10 lg:w-12 xl:w-14" />
           </button>
@@ -92,7 +92,7 @@ const Navbar = ({ loading }) => {
 
         <div>
           <Link to="/cart">
-            <CartWidget className="h-auto w-8 sm:w-10 md:w-12 lg:w-14 xl:w-16" />
+            <CartWidget className="h-auto w-8 sm:w-10 md:w-12 lg:w-14 xl:w-16 flex-grow" />
           </Link>
         </div>
       </div>
